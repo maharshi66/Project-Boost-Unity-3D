b6411dd97e85ad755078f3d830d7f4d3617bd2ea
Maharshi Shah <maharshishah@mail.usf.edu>
2020-06-12 15:43:30 +0530

    Added collision management, scene loading and Level 2

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
     AudioSource audioSource;
+	[SerializeField] float rcsThrust = 100f; //Reaction Control System
+	[SerializeField] float mainThrust = 100f;
 
-    // Start is called before the first frame update
-    void Start()
+	enum State 
+	{ 
+		ALIVE,
+		DYING,
+		TRANSCENDING
+	};
+
+	State state = State.ALIVE;
+
+	// Start is called before the first frame update
+	void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         audioSource= GetComponent<AudioSource>();
@@ -18,27 +27,54 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		Thrust();
-		Rotation();
+		if(state == State.ALIVE)
+		{
+			Thrust();
+			Rotation();
+		}
+
     }
 
-	private void Rotation()
+	void OnCollisionEnter(Collision collision)
 	{
-		if (Input.GetKey(KeyCode.A))
+		if(state != State.ALIVE) //ignore collision when dead
 		{
-			transform.Rotate(Vector3.forward);
+			return;
 		}
-		else if (Input.GetKey(KeyCode.D))
+
+		switch (collision.gameObject.tag)
 		{
-			transform.Rotate(-Vector3.forward);
+			case "Friendly":
+				//Do Nothing
+				break;
+			
+			case "Finish":
+				state = State.TRANSCENDING;
+				Invoke("LoadNextScene", 1f); //parameterize time
+				break;
+
+			default:
+				state = State.DYING;
+				Invoke("LoadStartScene", 1f); //parameterize time
+				break;
 		}
+	}
+
+	private void LoadStartScene()
+	{
+		SceneManager.LoadScene(0);
+	}
+
+	private void LoadNextScene()
+	{	
+		SceneManager.LoadScene(1);
 	}
 
 	private void Thrust()
 	{
 		if (Input.GetKey(KeyCode.W))
 		{
-			rigidBody.AddRelativeForce(Vector3.up);
+			rigidBody.AddRelativeForce(Vector3.up * mainThrust);
 			if (!audioSource.isPlaying)
 			{
 				audioSource.Play();
@@ -48,5 +84,21 @@ public class Rocket : MonoBehaviour
 		{
 			audioSource.Stop();
 		}
+	}
+	
+	private void Rotation()
+	{
+		rigidBody.freezeRotation = true;	//take manual control over rotation
+		float rotationThatFrame = Time.deltaTime * rcsThrust;
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(Vector3.forward * rotationThatFrame);
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(-Vector3.forward * rotationThatFrame);
+		}
+		rigidBody.freezeRotation = false;
 	}
 }
