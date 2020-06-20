0220a5403a861e4fa205474f04f69c53bb907792
Maharshi Shah <maharshishah@mail.usf.edu>
2020-06-20 19:32:20 +0530

    Added Oscillator script for obstacles using Mathf.Sin. Organized all asset folders

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+
+    //todo remove from inspector
+    [SerializeField]
+    [Range(0, 1)]
+    float movementFactor;
+    
+    Vector3 startingPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //protect against period = 0
+        if(period <= Mathf.Epsilon)
+		{
+            return;
+		}
+
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2f;
+        float rawSinWave = Mathf.Sin(cycles * tau); //about 6.28
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+        
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+	[SerializeField] float rcsThrust = 100f; //Reaction Control System
+	[SerializeField] float mainThrust = 100f;
+
+	[SerializeField] AudioClip mainEngine;
+	[SerializeField] AudioClip rocketDeath;
+	[SerializeField] AudioClip levelFinished;
+
+	[SerializeField] ParticleSystem mainEngineParticle;
+	[SerializeField] ParticleSystem successParticle;
+	[SerializeField] ParticleSystem deathParticle;
+
+	enum State 
+	{ 
+		ALIVE,
+		DYING,
+		TRANSCENDING
+	};
+
+	State state = State.ALIVE;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource= GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(state == State.ALIVE)
+		{
+			RespondToThrustInput();
+			RespondToRotationInput();
+		}
+    }
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if(state != State.ALIVE) //ignore collision when dead
+		{
+			return;
+		}
+
+		switch (collision.gameObject.tag)
+		{
+			case "Friendly":
+				//Do Nothing
+				break;
+
+			case "Finish":
+				StartSuccessSequence();
+				break;
+
+			default:
+				StartDeathSequence();
+				break;
+		}
+	}
+
+	private void StartDeathSequence()
+	{
+		state = State.DYING;
+		audioSource.Stop();
+		deathParticle.Play();
+		audioSource.PlayOneShot(rocketDeath);
+		Invoke("LoadStartScene", 1f); //parameterize time
+	}
+
+	private void StartSuccessSequence()
+	{
+		state = State.TRANSCENDING;
+		audioSource.Stop();
+		audioSource.PlayOneShot(levelFinished);
+		successParticle.Play();
+		Invoke("LoadNextScene", 1f); //parameterize time
+	}
+
+	private void LoadStartScene()
+	{
+		SceneManager.LoadScene(0);
+	}
+
+	private void LoadNextScene()
+	{	
+		SceneManager.LoadScene(1);
+	}
+
+	private void RespondToThrustInput()
+	{
+		if (Input.GetKey(KeyCode.W))
+		{
+			ApplyThrust();
+		}
+		else
+		{
+			audioSource.Stop();
+			mainEngineParticle.Stop();
+		}
+	}
+	private void ApplyThrust()
+	{
+		rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+		if (!audioSource.isPlaying)
+		{
+			audioSource.PlayOneShot(mainEngine);
+		}
+		mainEngineParticle.Play();
+	}
+	private void RespondToRotationInput()
+	{
+		rigidBody.freezeRotation = true;	//take manual control over rotation
+		float rotationThatFrame = Time.deltaTime * rcsThrust;
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(Vector3.forward * rotationThatFrame);
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(-Vector3.forward * rotationThatFrame);
+		}
+		rigidBody.freezeRotation = false;
+	}
+}
